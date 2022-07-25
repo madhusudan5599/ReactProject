d3f44b84c6ec02fb0872e8c07be22e8997212b0f
Madhusudan Bhanuse <madhusudan5599@gmail.com>
2022-07-25 20:33:11 +0530

    All files uploaded

---
+++ src/Components/Bascis/Menuapi.js
@@ -0,0 +1,81 @@
+const Menu = [{
+
+        id: 1,
+        image: "../Image/maggi.jpg",
+        category: "Breakfast",
+        name:"Maggi",
+        price : "200",
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    },
+    {
+
+        id: 2,
+        image: "../Image/maggi.jpg",
+        category: "Breakfast",
+        name:"Edli",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    },
+    {
+
+        id: 3,
+        image: "../Image/edli.jpg",
+        category: "lunch",
+        name:"Thali",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    },
+    {
+
+        id: 4,
+        image: "../Image/edli.jpg",
+        category: "dinner",
+        name:"Noodles",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    },
+    {
+
+        id: 5,
+        image: "../Image/edli.jpg",
+        category: "dinner",
+        name:"Noodles",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    }, {
+
+        id: 6,
+        image: "../Image/edli.jpg",
+        category: "dinner",
+        name:"Noodles",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    } ,{
+
+        id: 7,
+        image: "../Image/edli.jpg",
+        category: "dinner",
+        name:"Noodles",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    } ,{
+
+        id: 8,
+        image: "../Image/edli.jpg",
+        category: "dinner",
+        name:"Noodles",
+        price: '200',
+        discription: "sbddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+    }
+]
+
+
+export default Menu;
